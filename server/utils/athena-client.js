b58d66f7ee7f1df881bd97510442ce97af111300
Ram Mahajan <rammahajan7@gmail.com>
2018-04-09 17:42:50 +0800

    added aws config to test

--- server/utils/athena-client.js
+++ server/utils/athena-client.js
@@ -5,7 +5,9 @@ let clientConfig = {
   bucketUri: 's3://clp-hist-data/bike-journey/processed/data/'
 };
 let awsConfig = {
-  region: 'ap-southeast-1'
+  region: 'ap-southeast-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
 let client = athena.createClient(clientConfig, awsConfig);
 function getDataFromAthena(query) {
