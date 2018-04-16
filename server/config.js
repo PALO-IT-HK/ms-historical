1da0ffcbd5b949797bab48c0258f5a7e2ab20f0e
Beda Tse <beda@coeus.hk>
2018-04-16 12:38:11 +0800

    add athenaTable to config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   awsRegion: process.env.AWS_REGION || 'ap-southeast-1',
   athenaDb: process.env.ATHENA_DB || 'clp_bike_poc',
+  athenaTable: process.env.ATHENA_TABLE || 'journey_data',
   endpointBaseUrl: process.env.ENDPOINT_BASEINTERFACE || 'localhost:3000',
   athenaResultsBucket:
     process.env.ATHENA_RESULT_BUCKET ||
